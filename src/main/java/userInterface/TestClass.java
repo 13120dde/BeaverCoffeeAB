461adb3c054dadd03d80370f94f37f1579ab1d48
johanheld <johanheld@hotmail.com>
2018-05-23 15:58:36 +0200

    addOrder, increasePurchases

--- src/main/java/userInterface/TestClass.java
+++ src/main/java/userInterface/TestClass.java
@@ -1,12 +1,13 @@
 package userInterface;
 
 import database.MongoDb;
-import domainEntities.Comment;
-import domainEntities.Customer;
-import domainEntities.Employee;
+import domainEntities.*;
+import engine.Common;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class TestClass
 {
@@ -23,8 +24,25 @@ public class TestClass
 //            c.setDate(new Date());
 //            c.setEmployerId(22);
 //            db.addComment(c);
-            Customer c = new Customer();
-            db.addCustomer(c);
+//            Customer c = new Customer();
+//            c.setName("Bongokungen");
+//            c.setOccupation("Bagare");
+//            db.increasePurchases("12345");
+
+            List l = new ArrayList();
+            Product p = new Product("Kaffe", "Coffee", 10, 2, 3,
+                    "l", 2);
+
+            Product p2 = new Product("Sko", "Shoe", 10, 2, 3,
+                    "kg", 6);
+
+            l.add(p);
+            l.add(p2);
+            Common.setCurrentLocation(Location.SWEDEN);
+            Order o = new Order(12, 123, 432, new Date(), "SWEDEN", l);
+            db.addOrder(o);
+
+
 
         } catch (UnknownHostException e)
         {
